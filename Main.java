c9c7fb1d76d46c2f2aac59537973db16eb0d2a4a
nibrahi1 <32943276+nibrahi1@users.noreply.github.com>
2017-10-29 23:19:25 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,18 +1,31 @@
 package com.company;
+
 import java.util.Scanner;
+
+
+
 public class Main {
 
+
     public static void main(String[] args) {
-        int lottery= (int)(Math.random()*100000);
+        String prompt = "Please enter amount of money: ";
+        double amount = getAmount(prompt);
+
+
+        int lottery = (int) (Math.random() * 100000);
         Scanner input = new Scanner(System.in);
-        System.out.print("Enter lottery pick (five digits):");
+        String welcome = "";
+        System.out.println("Welcome to the Lottery Game, Please enter your name");
+        welcome = input.next();
+        System.out.print("Hello " + welcome.toUpperCase() + " Please enter lottery pick (five digits):");
         int guess = input.nextInt();
 
-        int lotteryDigit1= lottery / 10;
-        int lotteryDigit2= lottery / 10;
-        int lotteryDigit3= lottery / 10;
-        int lotteryDigit4= lottery / 10;
-        int lotteryDigit5= lottery % 10;
+
+        int lotteryDigit1 = lottery / 10;
+        int lotteryDigit2 = lottery / 10;
+        int lotteryDigit3 = lottery / 10;
+        int lotteryDigit4 = lottery / 10;
+        int lotteryDigit5 = lottery % 10;
 
         int guessDigit1 = guess / 10;
         int guessDigit2 = guess / 10;
@@ -25,10 +38,10 @@ public class Main {
         if (guess == lottery)
             System.out.println("Exact match: You win $100,000 :D");
         else if (guessDigit5 == lotteryDigit1
-             &&  guessDigit4 == lotteryDigit2
-             &&  guessDigit3 == lotteryDigit3
-             &&  guessDigit2 == lotteryDigit4
-             &&  guessDigit1 == lotteryDigit5)
+                && guessDigit4 == lotteryDigit2
+                && guessDigit3 == lotteryDigit3
+                && guessDigit2 == lotteryDigit4
+                && guessDigit1 == lotteryDigit5)
             System.out.println("Match all the digits: You win $25,000 :)");
         else if (guessDigit1 == lotteryDigit1
                 || guessDigit1 == lotteryDigit2
@@ -52,14 +65,23 @@ public class Main {
                 || guessDigit5 == lotteryDigit5)
             System.out.println("Match one digit: you win $1,000");
         else
-            System.out.println("Sorry, You lose :(");
-
-
-
-
-
-
+            for (int i = 0; i < 3; i++) {
+                System.out.println("Sorry, You lose :(");
+            }
 
 
     }
+
+    public static double getAmount(String prompt) {
+        Scanner input = new Scanner(System.in);
+        System.out.print(prompt);
+        double amount = input.nextDouble();
+        //input.close();
+        return amount;
+    }
 }
+
+
+
+
+
