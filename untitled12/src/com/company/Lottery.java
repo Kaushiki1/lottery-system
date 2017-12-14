9091815e613807464e959678ad76e138fb066302
dahn8472 <32944234+dahn8472@users.noreply.github.com>
2017-12-13 22:34:53 -0500

    Update Lottery.java

--- untitled12/src/com/company/Lottery.java
+++ untitled12/src/com/company/Lottery.java
@@ -4,6 +4,25 @@ public class Lottery extends Lotteries {
     private String number;
     private String name;
     private int digits;
+    
+    public String getNumber() {
+        return number;
+    }
+    public String getName() {
+        return name;
+    }
+    public int getDigits() {
+        return digits;
+    }
+    public void setNumber(String newNumber) {
+        number = newNumber;
+    }
+    public void setName(String newName) {
+        name = newName;
+    }
+    public void setDigits(int newDigits) {
+        digits = newDigits;
+    }    
 
     public Lottery(String n, String na){
         number = n;
